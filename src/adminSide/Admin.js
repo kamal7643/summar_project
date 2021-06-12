5dddb8ce5b5f7152470be7aaa722de861b65aba6
kamal7643 <kamalswami0808@gmail.com>
2021-06-12 19:45:49 +0530

    need users

--- src/adminSide/Admin.js
+++ src/adminSide/Admin.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import { useHistory } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Loading from '../components/Loading';
+import Popup from '../components/Popup';
 
 
 function Admin(props) {
+    const [isOpen, setisOpen] = useState(true);
     const history = useHistory();
+    const togglePopup = () => {
+        setisOpen(!isOpen);
+    }
 
     return(<div>
             <Header />
-            <div>
-                <ul className="list-group">
-                    <li className="list-group-item" onClick={() => { history.push("/admin/suggestion") }}>Suggestion</li>
-                    <li className="list-group-item" onClick={() => { history.push("/admin/events") }}>Event</li>
-                </ul>
+        {isOpen && <Popup
+            content={<>
+                <div style={{marginBottom:'200px'}}><Loading/></div>
+            </>}
+            handleClose={togglePopup}
+        />}
+            <div>{
+                // <ul className="list-group">
+                //     <li className="list-group-item" onClick={() => { history.push("/admin/suggestion") }}>Suggestion</li>
+                //     <li className="list-group-item" onClick={() => { history.push("/admin/events") }}>Event</li>
+                // </ul>
                 
-                
+                }
             </div>
         </div>);
 }
